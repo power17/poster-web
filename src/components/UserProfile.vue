49d15c7d4886b7c651b800dafe331bda0bfba6da
power <1410837981@qq.com>
2023-07-03 17:04:41 +0800

    feat： login登录功能实现

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,22 +1,27 @@
 <template>
-    <router-link to="/login">
+    <router-link to="/login" v-if="!isLogin">
         <a-button type="primary" class="user-profile-component">登录</a-button>
     </router-link>
-    <!-- <div>
+    <div v-else>
         <a-dropdown-button class="user-profile-component">
-            <router-link to="/setting">viket</router-link>
+            <router-link to="/">{{ userInfo.nickName }}</router-link>
             <template #overlay>
                 <a-menu class="user-profile-dropdown">
                     <a-menu-item key="0">登出</a-menu-item>
                 </a-menu>
             </template>
         </a-dropdown-button>
-    </div> -->
+    </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useUserStore } from '../store/user'
+import { computed } from 'vue'
+const userStore = useUserStore()
+const userInfo = computed(() => userStore.userInfo)
+const isLogin = computed(() => userStore.isLogin)
+</script>
 <style scoped lang="scss">
-.user-profile-component,
-.user-profile-dropdown {
+.user-profile-component {
     border-radius: 20px;
 }
 
