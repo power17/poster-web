42ff94aba9f3199a46f3e87278f315894d97d1c7
power <1410837981@qq.com>
2023-07-06 22:48:20 +0800

    feat: 属性设置折叠效果

--- src/components/EditGroup/index.vue
+++ src/components/EditGroup/index.vue
@@ -1,26 +1,29 @@
 <template>
     <div class="edit-groups">
-        <div v-for="(item, index) in newGrops" :key="index">
-            <div>{{ item.text }}</div>
-            <pre>{{ item.items }}</pre>
-        </div>
+        <a-collapse v-model:activeKey="activeKey">
+            <a-collapse-panel v-for="(item, key) in editGroups" :key="`item-${key}`" :header="item.text">
+                <select-table @change="handleChange" :data="item.propsMap"></select-table>
+            </a-collapse-panel>
+        </a-collapse>
     </div>
 </template>
 <script lang="ts"></script>
 <script setup lang="ts">
+import SelectTable from '../SelectTable/index.vue'
 import { AllComponentProps } from 'lego-bricks'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { difference } from 'lodash-es'
 import { defaultEditGroups } from './defaultEditGroup'
 export interface GroupProps {
     text: string
     items: string[]
 }
+const activeKey = ref('item-0')
 const props = withDefaults(defineProps<{ data: AllComponentProps; groups?: GroupProps[] }>(), {
     groups: defaultEditGroups,
 })
 
-const newGrops = computed(() => {
+const newGroups = computed(() => {
     const allNormalGroup = props.groups.reduce((pre, curr) => {
         return [...pre, ...curr.items]
     }, [] as string[])
@@ -33,4 +36,19 @@ const newGrops = computed(() => {
         ...props.groups,
     ]
 })
+const editGroups = computed(() => {
+    return newGroups.value.map((group) => {
+        const propsMap = {} as AllComponentProps
+        group.items.forEach((key) => {
+            propsMap[key] = props.data[key]
+        })
+        group.propsMap = propsMap
+        return group
+    })
+})
+const emits = defineEmits(['change'])
+const handleChange = ({ key, value }) => {
+    console.log({ key, value })
+    emits('change', { key, value })
+}
 </script>
